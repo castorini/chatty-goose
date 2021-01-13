233eece396f01138236895e31ef567ddd43c3f69
林聖傑 <jacklin_64@hotmail.com>
2021-01-13 16:47:30 +0000

    refactor hqe

--- cqr_hqe/retrieve_with_hqe.py
+++ cqr_hqe/retrieve_with_hqe.py
@@ -20,16 +20,10 @@ import time
 import json
 import numpy as np
 import collections
-from util import read_corpus, word_score, exact_key_word, query_expansion
-# Pyjnius setup
-import sys
-sys.path += ['../anserini/src/main/python'] # We assume you clone anserini outside of the current folder
-from pyjnius_setup import configure_classpath
-configure_classpath('../anserini') # We assume you clone anserini outside of the current folder
-from jnius import autoclass
-JString = autoclass('java.lang.String')
-JSearcher = autoclass('io.anserini.search.SimpleSearcher')
-
+# from util import read_corpus, word_score, exact_key_word, query_expansion
+from pyserini.search import SimpleSearcher
+from pyserini.index import IndexReader
+from hqe import HQE
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='HQE for CAsT.')
@@ -57,16 +51,17 @@ if __name__ == '__main__':
     parser.add_argument('--QR_method', default='hqe', help='input_query') #origin, concat, hqe
     parser.add_argument('--filter', default='pos', help='filter word method') #pos, no
     args = parser.parse_args()
-    searcher = JSearcher(JString(args.index))
-    searcher.setBM25Similarity(float(args.k1), float(args.b))
+    searcher = SimpleSearcher(args.index)
+    searcher.set_bm25(float(args.k1), float(args.b))
     print('Initializing BM25, setting k1={} and b={}'.format(args.k1, args.b))
     if args.rm3:
-        searcher.setRM3Reranker(args.fbTerms, args.fbDocs, args.originalQueryWeight)
+        searcher.set_rm3(args.fbTerms, args.fbDocs, args.originalQueryWeight)
         print('Initializing RM3, setting fbTerms={}, fbDocs={} and originalQueryWeight={}'.format(args.fbTerms, args.fbDocs, args.originalQueryWeight))
 
 
-
-    id_to_doc = read_corpus(args.corpus)
+    HQE_for_BM25 = HQE(args.M0, args.eta0, args.R0_topic, args.R0_sub, args.filter, True)
+    HQE_for_BERT = HQE(args.M1, args.eta1, args.R0_topic, args.R1_sub, args.filter)
+    # id_to_doc = read_corpus(args.corpus)
     with open(args.output+'.tsv', 'w') as fout0:
         with open(args.output+'.doc.tsv', 'w') as fout1:
 
@@ -74,19 +69,18 @@ if __name__ == '__main__':
             with open(args.qid_queries) as json_file:
                 data = json.load(json_file)
 
-
+            qr_total_time = 0
+            initial_time = time.time()
             for session in data:
                 session_num = str(session['number'])
                 # Since we use different hyperparameters for first stage and second stage retrieval, we maintain two keyword(sub keyword) lists
                 # For first stage
-                key_word_list = collections.defaultdict(list)
-                subkey_word_list = collections.defaultdict(list)
+                # key_word_list = collections.defaultdict(list)
+                # subkey_word_list = collections.defaultdict(list)
                 # For second stage
-                key_word_list1 = collections.defaultdict(list)
-                subkey_word_list1 = collections.defaultdict(list)
+                # key_word_list1 = collections.defaultdict(list)
+                # subkey_word_list1 = collections.defaultdict(list)
                 hist_query = []
-
-
 
                 start_time = time.time()
                 for turn_id, conversations in enumerate(session['turn']):
@@ -103,53 +97,54 @@ if __name__ == '__main__':
                     conversation_num = str(conversations['number'])
                     qid=session_num+"_"+conversation_num
 
+                    qr_start_time = time.time()
+                    # if (args.QR_method =='hqe'):
+                    #     hits = searcher.search(query, 1)
 
-
-                    if (args.QR_method =='hqe'):
-                        hits = searcher.search(JString(query.encode('utf8')), 1)
-
-                        proc_query = word_score(query, searcher, JString)
-
-
-
-                        # extract subtopic keyword
-                        subkey_word_list = exact_key_word(proc_query, turn_id, args.R0_sub, args.R0_topic, subkey_word_list, args.filter)
-                        subkey_word_list1 = exact_key_word(proc_query, turn_id, args.R1_sub, args.R1_topic, subkey_word_list1, args.filter)
+                    #     proc_query = word_score(query, searcher)
 
 
 
-                        # extract topic keyword
-                        key_word_list = exact_key_word(proc_query, turn_id, args.R0_topic, 100000, key_word_list, args.filter)
-                        key_word_list1 = exact_key_word(proc_query, turn_id, args.R1_topic, 100000, key_word_list1, args.filter)
+                    #     # extract subtopic keyword
+                    #     subkey_word_list = exact_key_word(proc_query, turn_id, args.R0_sub, args.R0_topic, subkey_word_list, args.filter)
+                    #     subkey_word_list1 = exact_key_word(proc_query, turn_id, args.R1_sub, args.R1_topic, subkey_word_list1, args.filter)
+
+
+
+                    #     # extract topic keyword
+                    #     key_word_list = exact_key_word(proc_query, turn_id, args.R0_topic, 100000, key_word_list, args.filter)
+                    #     key_word_list1 = exact_key_word(proc_query, turn_id, args.R1_topic, 100000, key_word_list1, args.filter)
 
 
 
 
 
-                        key_word = query_expansion(key_word_list, 0, turn_id+1)
-                        key_word1 = query_expansion(key_word_list1, 0, turn_id+1)
+                    #     key_word = query_expansion(key_word_list, 0, turn_id+1)
+                    #     key_word1 = query_expansion(key_word_list1, 0, turn_id+1)
 
-                        subkey_word = ''
-                        if not ( (hits[0].score > args.eta0) ):
-                            end_turn = turn_id+1
-                            start_turn = end_turn - args.M0
-                            if start_turn < 0:
-                                start_turn = 0
-                            subkey_word = query_expansion(subkey_word_list, start_turn, end_turn)
+                    #     subkey_word = ''
+                    #     if not ( (hits[0].score > args.eta0) ):
+                    #         end_turn = turn_id+1
+                    #         start_turn = end_turn - args.M0
+                    #         if start_turn < 0:
+                    #             start_turn = 0
+                    #         subkey_word = query_expansion(subkey_word_list, start_turn, end_turn)
 
-                        subkey_word1 = ''
-                        if not ( (hits[0].score > args.eta1) ):
-                            end_turn = turn_id+1
-                            start_turn = end_turn - args.M1
-                            if start_turn < 0:
-                                start_turn = 0
-                            subkey_word1 = query_expansion(subkey_word_list1, start_turn, end_turn)
+                    #     subkey_word1 = ''
+                    #     if not ( (hits[0].score > args.eta1) ):
+                    #         end_turn = turn_id+1
+                    #         start_turn = end_turn - args.M1
+                    #         if start_turn < 0:
+                    #             start_turn = 0
+                    #         subkey_word1 = query_expansion(subkey_word_list1, start_turn, end_turn)
 
-
+                    qr_total_time += time.time() - qr_start_time
                     # Generate Query for Anserini
                     if (args.QR_method =='hqe'):
-                        if turn_id!=0:
-                            query_for_anserini = key_word + ' ' + subkey_word + ' ' + query_for_anserini
+                        # if turn_id!=0:
+                        #     query_for_anserini = key_word + ' ' + subkey_word + ' ' + query_for_anserini
+                        query_for_anserini = HQE_for_BM25.rewrite(query, searcher)
+
                     elif args.QR_method =='origin':
                         query_for_anserini = query
                     elif args.QR_method=='concat':
@@ -168,8 +163,9 @@ if __name__ == '__main__':
 
                     # Generate Query for Bert
                     if (args.QR_method =='hqe'):
-                        if turn_id!=0:
-                            query_for_bert = key_word1 + ' ' + subkey_word1 + ' ' + query_for_bert
+                        # if turn_id!=0:
+                        #     query_for_bert = key_word1 + ' ' + subkey_word1 + ' ' + query_for_bert
+                        query_for_bert = HQE_for_BERT.rewrite(query, searcher)
                     elif args.QR_method =='origin':
                         query_for_bert = query
                     elif args.QR_method=='concat':
@@ -192,19 +188,24 @@ if __name__ == '__main__':
                         query_for_bert= hist_word + ' ' + query_for_bert
 
 
-                    hits = searcher.search(JString(query_for_anserini.encode('utf8')), int(args.hits))
+                    hits = searcher.search(query_for_anserini, int(args.hits))
                     for rank in range(len(hits)):
                         docno = hits[rank].docid
-                        doc = id_to_doc[docno]
+                        # doc = id_to_doc[docno]
 
 
                         fout0.write('{}\t{}\t{}\n'.format(qid, docno, rank + 1))
-                        fout1.write('{}\t{}\t{}\t{}\n'.format(qid, docno, query_for_bert, doc))
+                        # fout1.write('{}\t{}\t{}\t{}\n'.format(qid, docno, query_for_bert, doc))
 
+                HQE_for_BM25.reset_history()
+                HQE_for_BERT.reset_history()
 
                 time_per_query = (time.time() - start_time) / (turn_id + 1)
                 print('Retrieving session {} with {} queries ({:0.3f} s/query)'.format(session['number'], turn_id+1, time_per_query))
 
+            time_per_query = (time.time() - initial_time) / (total_query_count)
+            qr_time_per_query = qr_total_time / (total_query_count)
+            print('Retrieving {} queries ({:0.3f} s/query, QR {:0.3f} s/query)'.format(total_query_count, time_per_query, qr_time_per_query))
 
 
     print('total Query Counts %d'%(total_query_count))
--- cqr_hqe/util.py
+++ cqr_hqe/util.py
@@ -1,116 +1,19 @@
-import re
-import spacy
 
-nlp = spacy.load('en', parser = False)
+def reciprocal_rank_fusion(list1, list2, k=60):
+    """
+    Implements a reciprocal rank fusion as define in
+    ``Reciprocal Rank fusion outperforms Condorcet and individual Rank Learning Methods`` by Cormack, Clarke and Buettcher.
+    Parameters:
+        k: term to avoid vanishing importance of lower-ranked documents. Default value is 60 (default value used in their paper).
+        output: a file pointer to write the results. Sys.stdout is the default.
+    """
+    doc_scores = {}
+    for pos, docid in enumerate(list1, start=1):
+        doc_scores[docid] = doc_scores.get(docid, 0.0) + 1.0 / (k + pos)
 
-def space_extend( matchobj):
-    return ' ' + matchobj.group(0) + ' '
-def pre_proc(text):
-    text = re.sub(u'-|\u2010|\u2011|\u2012|\u2013|\u2014|\u2015|%|\[|\]|:|\(|\)|/|\t', space_extend, text)
-    text = text.strip(' \n')
-    text = re.sub('\s+', ' ', text)
-    return text
+    for pos, docid in enumerate(list2, start=1):
+        doc_scores[docid] = doc_scores.get(docid, 0.0) + 1.0 / (k + pos)
+    doc_scores= {k: v for k, v in sorted(doc_scores.items(), key=lambda item: item[1], reverse=True)}
 
-def _str(s):
-        """ Convert PTB tokens to normal tokens """
-        if (s.lower() == '-lrb-'):
-            s = '('
-        elif (s.lower() == '-rrb-'):
-            s = ')'
-        elif (s.lower() == '-lsb-'):
-            s = '['
-        elif (s.lower() == '-rsb-'):
-            s = ']'
-        elif (s.lower() == '-lcb-'):
-            s = '{'
-        elif (s.lower() == '-rcb-'):
-            s = '}'
-        return s
+    return [*doc_scores]
 
-def process(parsed_text):
-    output = {'word': [],
-              'lemma': [],
-              'pos': [],
-              'pos_id': [],
-              'ent': [],
-              'ent_id': [],
-              'offsets': [],
-              'sentences': []}
-
-    for token in parsed_text:
-        #[(token.text,token.idx) for token in parsed_sentence]
-        output['word'].append(_str(token.text))
-        pos = token.tag_
-        output['pos'].append(pos)
-        # output['pos_id'].append(token2id(pos, POS, 0))
-
-        ent = 'O' if token.ent_iob_ == 'O' else (token.ent_iob_ + '-' + token.ent_type_)
-        output['ent'].append(ent)
-        # output['ent_id'].append(token2id(ent, ENT, 0))
-
-        output['lemma'].append(token.lemma_ if token.lemma_ != '-PRON-' else token.text.lower())
-        output['offsets'].append((token.idx, token.idx + len(token.text)))
-
-    word_idx = 0
-    for sent in parsed_text.sents:
-        output['sentences'].append((word_idx, word_idx + len(sent)))
-        word_idx += len(sent)
-
-    assert word_idx == len(output['word'])
-    return output
-
-def word_score(query, searcher, JString):
-    nlp_query = nlp(pre_proc(query))
-    proc_query = process(nlp_query)
-    query_words = proc_query['word']
-    proc_query['score'] = []
-
-    for query_word in query_words:
-        hits = searcher.search(JString(query_word.encode('utf8')), 1)
-        try:
-            score = hits[0].score
-            proc_query['score'].append(score)
-        except:
-            proc_query['score'].append(-1)
-
-    return proc_query
-
-def read_corpus(corpus_path):
-    id_to_doc = {}
-    with open(corpus_path, 'r') as f:
-        print('Read Corpus...')
-        for line in f:
-            try:
-                text = line.strip().split("\t")
-                docid = text[0]
-                doc= ' '.join(text[1:])
-                id_to_doc[docid] = doc
-            except:
-                print("skip %s"%(line.strip()))
-
-    return id_to_doc
-
-
-def exact_key_word(proc_query, turn_id, low, up, key_word_list, filter):
-    if filter=='no':
-        for i, word in enumerate(proc_query['word']):
-            if ( (proc_query['score'][i]>=(low))&(proc_query['score'][i]<up)   ):
-                key_word_list[turn_id].append(word)
-    elif filter=='pos':
-        for i, word in enumerate(proc_query['word']):
-            if(('NN' in proc_query['pos'][i]) or ('JJ' in proc_query['pos'][i]) ):
-                if ( (proc_query['score'][i]>=(low))&(proc_query['score'][i]<up)   ):
-                    key_word_list[turn_id].append(word)
-    elif filter=='stp':
-        for i, word in enumerate(proc_query['word']):
-            if(word not in STOP_WORDS):
-                if ( (proc_query['score'][i]>=(low))&(proc_query['score'][i]<up)   ):
-                    key_word_list[turn_id].append(word)
-    return key_word_list
-
-def query_expansion(key_word_list, start_turn, end_turn):
-    query_expansion = ''
-    for turn in range(start_turn, end_turn):
-        for word in key_word_list[turn]:
-            query_expansion= query_expansion+' '+ word
-    return query_expansion
