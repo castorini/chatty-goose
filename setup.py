0cc4ff731bd6fc47cb588385121885a960a9c39a
Edwin Zhang <edwinzhang64@gmail.com>
2021-05-07 00:07:23 -0600

    Restrict SpaCy version and update package setup (#17)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ excluded = ["data*", "examples*", "experiments*"]
 
 setuptools.setup(
     name="chatty-goose",
-    version="0.1.0",
+    version="0.2.0",
     author="Anserini Gaggle",
     author_email="anserini.gaggle@gmail.com",
     description="A conversational passage retrieval toolkit",
